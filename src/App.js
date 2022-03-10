ffbd7ff3642e6866fa114f3f35cb9c9202f64d31
gerald <geraldcerd@gmail.com>
2022-03-10 13:28:47 -0600

    testing deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import image1 from './images/1.jpg'
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          <img src={image1}/>
         </a>
       </header>
     </div>
